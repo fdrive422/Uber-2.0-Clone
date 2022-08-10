19ebbb8ac1e2fbe30c937f40ff76aa1f03226403
fdrive422 <fafrago@gmail.com>
2022-08-09 22:45:03 -0700

    initial commit - setup provider

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+// 1) set up redux
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text> Let's build UBER!!! </Text>
+      </View>
+    </Provider>
   );
 }
 
